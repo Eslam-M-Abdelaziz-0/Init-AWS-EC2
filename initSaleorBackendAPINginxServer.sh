fc1932dcedd340ecb571dbe572ee4947cd6a81c1
IslamDEVO <i.mohammed14.44@outlook.com>
2021-06-27 23:27:42 +0200

    add initSaleorBackendAPI script

--- initSaleorBackendAPINginxServer.sh
+++ initSaleorBackendAPINginxServer.sh
@@ -34,3 +34,35 @@ sudo apt-get install libpangocairo-1.0-0 -y
  cd saleor
  git fetch origin 2.10:2.10 && git checkout 2.10
  pip install -r requirements.txt
+
+ ### settiing up postgres user.
+ sudo -u postgres psql -c "CREATE ROLE saleor WITH LOGIN PASSWORD 'saleor';"
+ sudo -u postgres psql -c "CREATE DATABASE saleor;"
+ sudo -u postgres psql -c "ALTER USER saleor WITH SUPERUSER;"
+
+
+IP="34.221.197.60"
+
+cd ~/code/backend/saleor
+source ~/code/backend/venv/bin/activate
+export ALLOWED_HOSTS=$IP
+export ALLOWED_CLIENT_HOSTS=$IP
+export DEBUG=True
+export SECRET_KEY=123456
+export INTERNAL_IPS=127.0.0.1,$IP,
+export API_URI=http://$IP:8000/graphql/
+export APP_MOUNT_URI=/dashboard/
+export DEFAULT_COUNTRY=IN
+export DEFAULT_CURRENCY=INR
+
+### Applying migrations
+ python manage.py migrate
+
+## create dummy data
+ python manage.py populatedb
+ 
+## Create superuser
+ python manage.py createsuperuser
+
+## runserver
+python manage.py runserver 0.0.0.0:8000
