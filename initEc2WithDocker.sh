f7e10590299d03a936463b7e4469ae3c9665a30d
IslamDEVO <i.mohammed14.44@outlook.com>
2021-06-27 20:52:47 +0200

    initEc2WithDocker

---
+++ initEc2WithDocker.sh
@@ -0,0 +1,27 @@
+
+sudo apt-get update
+cd ~
+mkdir -p code/
+cd code
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+# Install Docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Create the docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+
+# Install Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
