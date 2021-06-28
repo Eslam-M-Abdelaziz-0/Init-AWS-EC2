be40159f39d4c2f6cd75e1dc1d40f5c47c3c8720
IslamDEVO <i.mohammed14.44@outlook.com>
2021-06-28 02:23:20 +0200

    Add nginx Server

--- initNginx.sh
+++ initNginx.sh
@@ -1,16 +1,24 @@
+## Removes all but config files.
+ #sudo apt-get remove nginx nginx-common
+
+## Removes everything.
+ #sudo apt-get purge nginx nginx-common
+ #sudo apt-get autoremove
+
+
 
 sudo apt-get install nginx -y
-sudo /etc/init.d/nginx start    # start nginx
 
 source ~/code/backend/venv/bin/activate
 
-mv nginx-config-files/uwsgi_params ~/code/backend/saleor/uwsgi_params
+cp nginx-config-files/uwsgi_params ~/code/backend/saleor
 
-mv nginx-config-files/saleor.config /etc/nginx/sites-available/saleor.conf
+cp nginx-config-files/saleor.config /etc/nginx/sites-available/
 
 sudo ln -s /etc/nginx/sites-available/saleor.conf /etc/nginx/sites-enabled/
 
-sudo /etc/init.d/nginx restart # restart nginx
+sudo /etc/init.d/nginx start    # start nginx
+#sudo /etc/init.d/nginx restart # restart nginx
 
 cd ~/code/backend/saleor
 uwsgi --socket mysite.sock --module saleor.wsgi --chmod-socket=666
