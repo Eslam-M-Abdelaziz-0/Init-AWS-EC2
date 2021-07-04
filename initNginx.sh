4729b9d7c37ab545ff52657beb3f8d99cc869088
IslamDEVO <i.mohammed14.44@outlook.com>
2021-07-04 21:36:33 +0200

    add initEc2DockerCompose

--- initNginx.sh
+++ initNginx.sh
@@ -15,10 +15,12 @@ cp nginx-config-files/uwsgi_params ~/code/backend/saleor
 
 sudo cp nginx-config-files/saleor.config /etc/nginx/sites-available/
 
-sudo ln -s /etc/nginx/sites-available/saleor.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/saleor.config /etc/nginx/sites-enabled/
 
 sudo /etc/init.d/nginx start    # start nginx
 #sudo /etc/init.d/nginx restart # restart nginx
 
 cd ~/code/backend/saleor
 uwsgi --socket mysite.sock --module saleor.wsgi --chmod-socket=666
+
+#uwsgi --http :8000 --module saleor.wsgi
