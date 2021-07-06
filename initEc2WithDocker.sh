4b5b1c267070e9a2e96546ec33bfcd4d6bda39e3
IslamDEVO <i.mohammed14.44@outlook.com>
2021-07-06 20:06:35 +0200

    change command for install docker-compose

--- initEc2WithDocker.sh
+++ initEc2WithDocker.sh
@@ -23,6 +23,4 @@ sudo usermod -aG docker $USER
 newgrp docker 
 
 # Install Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo apt  install docker-compose -y
